357436d9164ba69ae1d108e15ebc89e3a3167223
Avaer Kazmer <a@modules.io>
2022-09-29 20:47:50 -0400

    Update build

--- api/index.js
+++ api/index.js
@@ -1,26 +1,14 @@
 import chrome from 'chrome-aws-lambda'
 import { chromium } from 'playwright-core'
-import { serverTiming } from './../lib/helpers.js'
-import Fastify from 'fastify'
+// import { serverTiming } from './../lib/helpers.js'
 
-const app = Fastify({
-  logger: process.env.NODE_ENV === 'development' ? true : false,
-})
-
-const allowedOrigins = ['localhost', 'briefkastenhq.com', 'ndo.dev']
-
-const resolveOrigin = (origin) => {
-  if (allowedOrigins.includes(new URL(origin).hostname)) {
-    return origin
-  }
-  return ''
-}
+const app = express();
 
 const handleScreenshot = async ({ params, url }) => {
-  const { colorScheme, skipCookieBannerClick } = params
+  // const { colorScheme, skipCookieBannerClick } = params
 
-  serverTiming.start()
-  serverTiming.measure('browserStart')
+  // serverTiming.start()
+  // serverTiming.measure('browserStart')
   const browser = await chromium.launch({
     args: chrome.args,
     executablePath:
@@ -36,42 +24,18 @@ const handleScreenshot = async ({ params, url }) => {
     height: 1080,
     deviceScaleFactor: 1,
   })
-  serverTiming.measure('browserStart')
-  serverTiming.measure('pageView')
-  if (colorScheme) {
-    await page.emulateMedia({ colorScheme })
-  }
+  // serverTiming.measure('browserStart')
+  // serverTiming.measure('pageView')
+  // if (colorScheme) {
+    // await page.emulateMedia({ colorScheme })
+  // }
   await page.goto(url)
-  serverTiming.measure('pageView')
+  // serverTiming.measure('pageView')
 
-  // Hack for accepting cookie banners
-  if (!skipCookieBannerClick) {
-    const selectors = [
-      '[id*=cookie] a',
-      '[class*=consent] button',
-      '[class*=cookie] a',
-      '[id*=cookie] button',
-      '[class*=cookie] button',
-    ]
-
-    const regex =
-      /(Accept all|I agree|Accept|Agree|Agree all|Ich stimme zu|Okay|OK)/
-
-    serverTiming.measure('cookieHack')
-    const elements = await page.$(`'${selectors.join(', ')}'`)
-    if (elements) {
-      for (const el of elements) {
-        const innerText = (await el.getProperty('innerText')).toString()
-        regex.test(innerText, 'ig') && el.click()
-      }
-    }
-
-    serverTiming.measure('cookieHack')
-  }
-  serverTiming.measure('screenshot')
+  // serverTiming.measure('screenshot')
   // Snap screenshot
   const buffer = await page.screenshot({ type: 'png' })
-  serverTiming.measure('screenshot')
+  // serverTiming.measure('screenshot')
 
   await page.close()
   await browser.close()
@@ -79,88 +43,16 @@ const handleScreenshot = async ({ params, url }) => {
   return buffer
 }
 
-app.get('/', async () => {
-  return { hello: 'world' }
-})
-
-/**
- * @type {import('fastify').RouteShorthandOptions}
- */
-app.get('/api/image', async (req, reply) => {
+export default async (req, reply) => {
   let { url } = req.query
 
-  if (req.headers.origin) {
-    reply.header(
-      'Access-Control-Allow-Origin',
-      resolveOrigin(req.headers.origin)
-    )
-  }
+  // Set the `s-maxage` property to cache at the CDN layer
+  // reply.header('Cache-Control', 's-maxage=31536000, public')
+  // reply.header('Content-Type', 'image/png')
 
-  if (!url) {
-    url = req.body?.url ?? undefined
-    reply.header('Access-Control-Allow-Headers', 'Content-Type')
-    reply.header('Content-Type', 'application/json')
-    throw { statusCode: 400, message: 'Missing URL Param' }
-  }
-
-  try {
-    // Set the `s-maxage` property to cache at the CDN layer
-    reply.header('Cache-Control', 's-maxage=31536000, public')
-    reply.header('Content-Type', 'image/png')
-
-    // Generate Server-Timing headers
-    const imageBuffer = await handleScreenshot({ params: req.query, url })
-    reply.header('Server-Timing', serverTiming.setHeader())
-    return imageBuffer
-  } catch (e) {
-    console.error('Error generating screenshot -', e)
-    return JSON.stringify({
-      message: 'Image Capture Failed',
-      error: e,
-    })
-  }
-})
-
-/**
- * @type {import('fastify').RouteShorthandOptions}
- */
-app.post('/api/image', async (req, reply) => {
-  let { url } = req.query
-
-  if (!url) {
-    url = req.body?.url ?? undefined
-  }
-  if (req.headers.origin) {
-    reply.header(
-      'Access-Control-Allow-Origin',
-      resolveOrigin(req.headers.origin)
-    )
-  }
-  if (!url) {
-    reply.header('Access-Control-Allow-Headers', 'Content-Type')
-    reply.header('Content-Type', 'application/json')
-    throw { statusCode: 400, message: 'Missing URL Param' }
-  }
-
-  try {
-    // Set the `s-maxage` property to cache at the CDN layer
-    reply.header('Cache-Control', 's-maxage=31536000, public')
-    reply.header('Content-Type', 'image/png')
-
-    // Generate Server-Timing headers
-    reply.header('Server-Timing', serverTiming.setHeader())
-
-    return handleScreenshot({ params: req.query, url })
-  } catch (e) {
-    console.error('Error generating screenshot -', e)
-    return JSON.stringify({
-      message: 'Image Capture Failed',
-      error: e,
-    })
-  }
-})
-
-export default async (req, res) => {
-  await app.ready()
-  app.server.emit('request', req, res)
+  // Generate Server-Timing headers
+  const imageBuffer = await handleScreenshot({ params: req.query, url })
+  // reply.header('Server-Timing', serverTiming.setHeader())
+  reply.setHeader('Content-Type', 'image/png')
+  reply.send(imageBuffer)
 }
