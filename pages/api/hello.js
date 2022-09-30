edf7b152e0644e360f8b6ed2df9bc1b85561a9e1
Avaer Kazmer <a@modules.io>
2022-09-29 20:20:08 -0400

    Add new files

---
+++ pages/api/hello.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const playwright = require("playwright-aws-lambda");
+
+export default async function handler(req, res) {
+  const browser = await playwright.launchChromium({ headless: true });
+  const context = await browser.newContext({
+    viewport: { width: 1024, height: 1024 },
+  });
+
+  const page = await context.newPage();
+  await page.goto("https://en.wikipedia.org/", {
+    waitUntil: "networkidle0",
+  });
+
+  const buffer = await page.screenshot();
+
+  res.setHeader("Content-Type", "image/png");
+  res.send(buffer);
+
+  await context.close();
+  await browser.close();
+};
