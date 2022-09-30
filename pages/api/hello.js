91e8fcc62fdcc1d1ef77fd6617308a6ee018a7d8
Avaer Kazmer <a@modules.io>
2022-09-29 20:31:23 -0400

    Clean up async wrapping

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,30 +2,32 @@
 // import chrome from "chrome-aws-lambda";
 // import puppeteer from "puppeteer-core";
 
-export default async function handler(req, res) {
-  const chrome = await import("chrome-aws-lambda");
-  console.log('imported chrome', chrome);
-  const puppeteer = await import("puppeteer-core");
-  console.log('imported puppeteer', puppeteer);
-  const options = {
-    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chrome.defaultViewport,
-    executablePath: await chrome.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  };
-  console.log("trying to execute");
-  const browser = await puppeteer.launch(options);
-  console.log("browser launched");
-  const page = await browser.newPage();
-  await page.setViewport({ width: 2000, height: 1000 });
-  await page.goto("https://en.wikipedia.org/", {
-    waitUntil: "networkidle0",
-  });
-  const buffer = await page.screenshot();
+export default function handler(req, res) {
+  (async () => {
+    const chrome = await import("chrome-aws-lambda");
+    console.log('imported chrome', chrome);
+    const puppeteer = await import("puppeteer-core");
+    console.log('imported puppeteer', puppeteer);
+    const options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+    console.log("trying to execute");
+    const browser = await puppeteer.launch(options);
+    console.log("browser launched");
+    const page = await browser.newPage();
+    await page.setViewport({ width: 2000, height: 1000 });
+    await page.goto("https://en.wikipedia.org/", {
+      waitUntil: "networkidle0",
+    });
+    const buffer = await page.screenshot();
 
-  res.setHeader("Content-Type", "image/png");
-  res.send(buffer);
+    res.setHeader("Content-Type", "image/png");
+    res.send(buffer);
 
-  await browser.close();
+    await browser.close();
+  })();
 }
