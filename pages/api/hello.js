4853d19e5f89edb21a17a455384bc3b34a6d51b4
Avaer Kazmer <a@modules.io>
2022-09-29 20:04:32 -0400

    Update screenshot route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const options = {
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
+  console.log("trying to execute");
+  const browser = await puppeteer.launch(options);
+  console.log("browser launched");
+  const page = await browser.newPage();
+  await page.setViewport({ width: 2000, height: 1000 });
+  await page.goto("https://en.wikipedia.org/", {
+    waitUntil: "networkidle0",
+  });
+  await page.screenshot({ path: "bexer.png" });
+
+  await browser.close();
 }
